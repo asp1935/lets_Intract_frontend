1770285d72991977e1b1a9fb5a7d8e283b9785c9
asp1935 <akashp1935@gmail.com>
2025-04-05 14:22:04 +0530

    otp related changes for dev

--- src/hooks/useOtp.js
+++ src/hooks/useOtp.js
@@ -4,8 +4,8 @@ import { sendOtp } from "../api/otpApi";
 export const useSendOtp = () => {
     return useMutation({
         mutationFn: (mobile) => sendOtp(mobile),
-        onSuccess: () => {
-            console.log('OTP Sent');
+        onSuccess: (data) => {
+            console.log('OTP Sent',data?.data?.otp);
         },
     });
 };
